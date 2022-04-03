63285588c259c328cf631a94ea22bd6474222c3e
Wektor607 <42221181+Wektor607@users.noreply.github.com>
2022-04-03 17:21:05 +0300

    Delete testing.py

--- LKH_wrapper/python-obertka/testing.py
+++
@@ -1,9 +0,0 @@
-import cvrp
-#import helloworld
-help(cvrp)
-#print(helloworld.parseOneTownPy("/Users/ivandruzkov/project/LKH0/20(20_задач)/20200925_093755.csv", "ttt/test0", 21, 60))
-
-# cvrp.createBinFile("20(20 задач)/20200925_093755_dist.csv", "/test", 21, 60)
-# cvrp.parseOneTownPy("20(20 задач)/20200925_093755.csv", "ttt_cvrp/test0", 21)
-# cvrp.modelMetaHeuristic("cvrp_lkh", "ttt_cvrp/test0", 21, 30)
-
